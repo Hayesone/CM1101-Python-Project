9db3c40214df4742913fd213e5fac51b8524f31e
Hayesone <joehayesone@gmail.com>
2018-10-19 01:08:24 +0100

    Changed from zip to folders

---
+++ game2_template/player.py
@@ -0,0 +1,10 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+# Start game at the reception
+current_room = rooms["Reception"]
+
+#sets the mass of the player#
+current_mass = 7.5
